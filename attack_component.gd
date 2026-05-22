76e277029b478df2a12625407c0c5e0edb7e59b0
kwochengho <kwochengho1020318@gmail.com>
2026-05-22 20:06:13 +0800

    attackComponent

--- attack_component.gd
+++ attack_component.gd
@@ -2,13 +2,9 @@ extends Node2D
 class_name Attack_Component
 
 func area_facing(direction)->void:
-	if !$AttackArea/CollisionArea:
-		print(get_node("AttackArea/CollisionArea"))
-		return
+	
 	$AttackArea/CollisionArea.position.x = abs($AttackArea/CollisionArea.position.x)
 	$DetectArea/CollisionArea.position.x = abs($DetectArea/CollisionArea.position.x)
 func area_valid(v)->void:
-	if !$AttackArea/CollisionArea:
-		print($AttackArea/CollisionArea)
-		return
+	
 	$AttackArea/CollisionArea.disabled=v
